13175859af84ec1349e680f1d5e3cf198d1e7ecc
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-10-03 12:40:38 +0200

    get token back for auth

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Login.css";
 import Logo from "../assets/Logo/logo.png";
 import { loginUrl } from "../spotify";
+import { getTokenFromUrl } from "../spotify";
 
 function Login() {
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    console.log("I have a token >>>", token);
+  }, []);
+
   return (
     <div className="login">
       <img src={Logo} alt="logo" />
--- src/spotify.js
+++ src/spotify.js
@@ -13,6 +13,20 @@ const scopes = [
   "user-modify-playback-state",
 ];
 
+// save access token from the response
+// "#access_token=BQC8p0O9bf8zna8E-0WdJ1fVCPS5bA7yqCIKxjxM3W-fH4kJnEtJLf4F_YA25cEBLzvv8eJTUKniHsbYz8e6PKShDhUCdQzP0tvEonwNfAJhqjysUUJfkNLiKF7s2Xm-j1cHNaKxEYvInwx8Sl8_m3oaxARmk_4TlDOtHQ&token_type=Bearer&expires_in=3600"
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      // # accessToken=
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
+};
+
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
