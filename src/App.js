d89ddf859aabba3d9dd4ab3f58712ff3412ed3c0
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-10-03 13:24:34 +0200

    setup spotify web api and basic of player component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Login from "./pages/Login";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./pages/Player";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -11,12 +15,17 @@ function App() {
     //console.log("I have a token >>>", hash);
     window.location.hash = "";
     const _token = hash.access_token;
-    _token && setToken(_token);
+    if (_token) {
+      setToken(_token);
+      // give our token to spotify api to connect app - spotify
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
+    }
   }, []);
 
-  return (
-    <div className="App">{token ? <h1> I am logged in</h1> : <Login />}</div>
-  );
+  return <div className="App">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
