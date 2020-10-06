b11dffcd92959fada032c20c0dca4b328ccd4af7
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-10-06 10:58:56 +0200

    impl footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,53 @@
 import React from "react";
 import "./Footer.css";
 
+import PlayCircleOutlineOutlinedIcon from "@material-ui/icons/PlayCircleOutlineOutlined";
+import SkipPreviousOutlinedIcon from "@material-ui/icons/SkipPreviousOutlined";
+import SkipNextOutlinedIcon from "@material-ui/icons/SkipNextOutlined";
+import ShuffleOutlinedIcon from "@material-ui/icons/ShuffleOutlined";
+import RepeatOutlinedIcon from "@material-ui/icons/RepeatOutlined";
+// slider
+import { Slider, Grid } from "@material-ui/core/";
+import VolumeDown from "@material-ui/icons/VolumeDown";
+import PlaylistPlayOutlinedIcon from "@material-ui/icons/PlaylistPlayOutlined";
+
 function Footer() {
   return (
     <div className="footer">
-      <h1>I am a footer</h1>
+      <div className="footer__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/b/b4/Metallica_-_Load_cover.jpg"
+          alt="cover"
+          className="footer__albumLogo"
+        />
+        <div className="footer__songInfo">
+          <h4>No song is playing</h4>
+          <p>Hello this is just test</p>
+        </div>
+      </div>
+      <div className="footer__center">
+        <ShuffleOutlinedIcon className="footer__green" />
+        <SkipPreviousOutlinedIcon className="footer__icon" />
+        <PlayCircleOutlineOutlinedIcon
+          fontSize="large"
+          className="footer__icon"
+        />
+        <SkipNextOutlinedIcon className="footer__icon" />
+        <RepeatOutlinedIcon className="footer__green" />
+      </div>
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayOutlinedIcon className="footer__volume" />
+          </Grid>
+          <Grid item>
+            <VolumeDown className="footer__volume" />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
