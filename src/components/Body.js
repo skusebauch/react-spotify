07ce450df983834b8bf79fb4cf6afac496a5fd4c
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-10-06 15:09:27 +0200

    styled body icons

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,12 @@ import { useStateValue } from "../store/StateProvider";
 import "./Body.css";
 import Header from "./Header";
 
+import {
+  FavoriteOutlined,
+  MoreHorizOutlined,
+  PlayCircleFilled,
+} from "@material-ui/icons";
+
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useStateValue();
   return (
@@ -15,6 +21,17 @@ function Body({ spotify }) {
           <h2>{discover_weekly?.name}</h2>
           <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilled className="body__shuffle" />
+          <FavoriteOutlined fontSize="large" />
+          <MoreHorizOutlined />
+        </div>
+        {/* List of songs */}
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow track={item.track} />
+        ))}
       </div>
     </div>
   );
